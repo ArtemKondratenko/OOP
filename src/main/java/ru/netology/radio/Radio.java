1c7175747df932729440acd85bf2f60a1e1fb900
Артем Кондратенко <artem-osteo@yandex.ru>
2022-06-09 16:52:54 +0300

    Add Radio

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,4 +1,26 @@
 package ru.netology.radio;
 
 public class Radio {
+
+    private int radioStation;
+    private int volume;
+
+    public int getRadioStation() {
+        return radioStation;
+    }
+    public void setRadioStation(int RadioStation) {
+        if (RadioStation < 0) {
+            return;
+        }
+        if (RadioStation > 9) {
+            return;
+        }
+        this.radioStation= RadioStation;
+    }
+
+    public void setNextStation(int RadioStation){
+        if (RadioStation == 9) {
+            return;
+        }
+    }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,7 +1,31 @@
 package ru.netology.radio;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadioTest {
+public class RadioTest {
 
+    @Test
+    public void limitRadioStation() {
+        Radio rad = new Radio();
+
+        rad.setRadioStation(5);
+
+        int expected = 5;
+        int actual = rad.getRadioStation();
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void nextRadioStation() {
+        Radio rad = new Radio();
+
+        rad.setNextStation(5);
+
+        int expected = 5;
+        int actual = rad.getRadioStation();
+
+        assertEquals(expected,actual);
+    }
 }
