a6f2bf2979e1b3c0d184f5209a822aaf24f93ca0
Артем Кондратенко <artem-osteo@yandex.ru>
2022-06-10 13:52:27 +0300

    Test

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -8,6 +8,7 @@ public class Radio {
     public int getRadioStation() {
         return radioStation;
     }
+
     public void setRadioStation(int RadioStation) {
         if (RadioStation < 0) {
             return;
@@ -15,12 +16,51 @@ public class Radio {
         if (RadioStation > 9) {
             return;
         }
-        this.radioStation= RadioStation;
+        this.radioStation = RadioStation;
     }
 
-    public void setNextStation(int RadioStation){
-        if (RadioStation == 9) {
+    public void nextStation() {
+        if (radioStation == 9) {
+            radioStation = 0;
+        } else {
+            radioStation = radioStation + 1;
+            setRadioStation(radioStation);
+        }
+    }
+
+    public void prevStation() {
+        if (radioStation == 0) {
+            radioStation = 9;
+        } else {
+            radioStation = radioStation - 1;
+            setRadioStation(radioStation);
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (volume < 0) {
             return;
+        }
+        if (volume > 9) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    public void maxVolume() {
+        if (volume == 9) {
+            setVolume(volume);
+        }
+    }
+
+    public void minVolume() {
+        if (volume == 0) {
+            setVolume(volume);
+
         }
     }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -15,17 +15,72 @@ public class RadioTest {
         int expected = 5;
         int actual = rad.getRadioStation();
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void nextRadioStation() {
         Radio rad = new Radio();
+        rad.setRadioStation(9);
 
-        rad.setNextStation(5);
+        rad.nextStation();
 
-        int expected = 5;
+        int expected = 0;
         int actual = rad.getRadioStation();
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
+    @Test
+    public void prevRadioStation() {
+        Radio rad = new Radio();
+        rad.setRadioStation(0);
+
+        rad.prevStation();
+
+        int expected = 9;
+        int actual = rad.getRadioStation();
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void limitVolume() {
+        Radio vol = new Radio();
+
+        vol.setVolume(5);
+
+        int expected = 5;
+        int actual = vol.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkPlusVolume() {
+        Radio vol = new Radio();
+        vol.setVolume(9);
+
+        vol.maxVolume();
+
+        int expected = 9;
+        int actual = vol.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkNegativeVolume() {
+        Radio vol = new Radio();
+        vol.setRadioStation(0);
+
+        vol.minVolume();
+
+        int expected = 0;
+        int actual = vol.getVolume();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
