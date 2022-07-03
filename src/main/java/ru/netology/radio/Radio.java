380e031bb0fa3bb050605751e55f9c75e748a550
Артем Кондратенко <artem-osteo@yandex.ru>
2022-07-03 13:27:06 +0300

    fix bug

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -4,40 +4,33 @@ public class Radio {
 
     private Integer radioStation;
     private Integer volume;
-    private Integer maxStation = 10;
     private Integer minStation = 0;
-    private Integer numberRadioStations;
-    private  Integer maxVolume = 100;
-    private  Integer minVolume = 0;
+    private Integer numberRadioStations = 10;
+    private Integer maxStation = numberRadioStations - 1;
+    private Integer maxVolume = 100;
+    private Integer minVolume = 0;
 
     public Radio(Integer numberRadioStations) {
-        this.radioStation = numberRadioStations;
-    }
-    public  Radio(){
-
+        this.numberRadioStations = numberRadioStations;
     }
 
+    public Radio() {
+    }
 
 
     public Integer getRadioStation() {
         return radioStation;
     }
 
-    public void setRadioStation(int RadioStation) {
-        if (RadioStation < 0 || RadioStation > 9) {
-            radioStation = null;
+    public void setRadioStation(int radioStation) {
+        if (radioStation < 0 || radioStation >= numberRadioStations) {
+            radioStation = 0;
         }
-        if (RadioStation == 10) {
-            radioStation = 9;
-        }
-        else {
-            this.radioStation = RadioStation;
-        }
-
+        this.radioStation = radioStation;
     }
 
     public void nextStation() {
-        if (radioStation == 9) {
+        if (radioStation == numberRadioStations - 1) {
             radioStation = minStation;
         }
     }
@@ -54,23 +47,21 @@ public class Radio {
 
     public void setVolume(int volume) {
         if (volume < minVolume || volume > maxVolume) {
-            this.volume = null;
-        }
-        else {
+            this.volume = 0;
+        } else {
             this.volume = volume;
         }
-
     }
 
     public void increaseVolume() {
-        if (volume == maxVolume) {
-            this.volume = null;
+        if (volume < maxVolume) {
+            this.volume++;
         }
     }
 
     public void decreaseVolume() {
-        if (volume == minVolume) {
-            this.volume = null;
+        if (volume > minVolume) {
+            this.volume--;
         }
     }
 }
