5370758de0ebe6ec7bf92c86bccc3a7a395ddd71
Артем Кондратенко <artem-osteo@yandex.ru>
2023-03-10 21:12:27 +0300

    NewFalie

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -2,43 +2,17 @@ package ru.netology.radio;
 
 public class Radio {
 
-<<<<<<< Updated upstream
-    private Integer radioStation;
-    private Integer volume;
-    private Integer minStation = 0;
-    private Integer numberRadioStations = 10;
-    private Integer maxStation = numberRadioStations - 1;
-    private Integer maxVolume = 100;
-    private Integer minVolume = 0;
-=======
     private int radioStation = 10;
     private int volume;
     private  int minRadioStation = 0;
     private int maxRadioStation = radioStation - 1;
     private  int minVolume = 0;
     private  int maxVolume = 100;
->>>>>>> Stashed changes
 
-    public Radio(Integer numberRadioStations) {
-        this.numberRadioStations = numberRadioStations;
-    }
-
-    public Radio() {
-    }
-
-<<<<<<< Updated upstream
-    public Integer getNumberRadioStations() {
-        return numberRadioStations;
-    }
-
-    public Integer getRadioStation() {
+    public int getRadioStation() {
         return radioStation;
     }
 
-    public void setRadioStation(int radioStation) {
-        if (radioStation < 0 || radioStation >= numberRadioStations) {
-            radioStation = 0;
-=======
     public Radio(int desiredNumberRadioStations) {
         this.radioStation = desiredNumberRadioStations;
     }
@@ -54,77 +28,52 @@ public class Radio {
         }
         else {
             this.radioStation = radioStation;
->>>>>>> Stashed changes
         }
-        this.radioStation = radioStation;
     }
 
     public void nextStation() {
-<<<<<<< Updated upstream
-        if (radioStation == numberRadioStations - 1) {
-            radioStation = minStation;
-        } else {
-            radioStation++;
-=======
         if (radioStation == maxRadioStation) {
             this.radioStation = 0;
         } else {
             this.radioStation = radioStation + 1;
             setRadioStation(radioStation);
->>>>>>> Stashed changes
         }
     }
 
     public void prevStation() {
-<<<<<<< Updated upstream
-        if (radioStation == 0) {
-            radioStation = maxStation;
-=======
         if (radioStation == minRadioStation) {
             radioStation = maxRadioStation;
->>>>>>> Stashed changes
         } else {
-            radioStation--;
+            radioStation = radioStation - 1;
+            setRadioStation(radioStation);
         }
     }
 
-    public Integer getVolume() {
+    public int getVolume() {
         return volume;
     }
 
     public void setVolume(int volume) {
         if (volume < minVolume || volume > maxVolume) {
-<<<<<<< Updated upstream
-            this.volume = 0;
-        } else {
-=======
             return;
         }
         else {
->>>>>>> Stashed changes
             this.volume = volume;
         }
     }
 
     public void increaseVolume() {
-<<<<<<< Updated upstream
-        if (volume < maxVolume) {
-            this.volume++;
-=======
         if (volume == maxVolume) {
             return;
->>>>>>> Stashed changes
         }
+        volume = volume + 1;
+        setVolume(volume);
     }
 
     public void decreaseVolume() {
         if (volume > minVolume) {
-<<<<<<< Updated upstream
-            this.volume--;
-=======
             volume = volume - 1;
             setVolume(volume);
->>>>>>> Stashed changes
         }
     }
 }
